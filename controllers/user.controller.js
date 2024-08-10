23e2a8f939f654cf3eb6beddf1248fb114a63b42
Ankit kumar <73269179+ankit-lang@users.noreply.github.com>
2024-08-10 05:55:28 +0000

    added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -116,7 +116,7 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
-        httpsOnly: true,
+   
        
       })
       .json({
